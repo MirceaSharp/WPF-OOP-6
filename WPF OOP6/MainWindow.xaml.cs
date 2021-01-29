3e4846a109e2b763770c6b45b4b07b30fdad7d87
MirceaSharp <mircea_vasile_petrescu@yahoo.com>
2021-01-29 15:03:18 +0100

    Added boiler functionality

--- WPF OOP6/MainWindow.xaml.cs
+++ WPF OOP6/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPF_OOP6
 {
@@ -23,6 +12,91 @@ namespace WPF_OOP6
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+
+
+        private void rdSony_Checked(object sender, RoutedEventArgs e)
+        {
+            TV Sony = new TV("Sony", "KD-77AG9", 100, 85, "sony.jpg");
+
+            if (chkPower.IsChecked == true)
+            {
+                BitmapImage pic = new BitmapImage();
+                pic.BeginInit();
+                pic.UriSource = new Uri(@"Images\" + "sony.jpg", UriKind.Relative);
+                pic.EndInit();
+                lblBrand.Content = "Brand     " + Sony.Brand;
+                lblType.Content = "Type   " + Sony.Type;
+                lblPower.Content = "Power    " + true;
+                lblHerz.Content = "Herz     " + Sony.Herz;
+                lblImageSize.Content = "Image Size    " + Sony.ImageSize;
+                lblChannel.Content = "Channel      " + Sony.Channel;
+                lblVolume.Content = "Volume       " + Sony.Volume;
+                img.Source = pic;
+            }
+            else
+            {
+
+
+                lblPower.Content = false;
+            }
+
+        }
+
+        private void rdSamsung_Checked(object sender, RoutedEventArgs e)
+        {
+            TV Samsung = new TV("Samsung", "QLED 8K 85Q950TS (2020)", 100, 77, "samsung.jpg");
+
+            if (chkPower.IsChecked == true)
+            {
+                BitmapImage pic2 = new BitmapImage();
+                pic2.BeginInit();
+                pic2.UriSource = new Uri(@"Images\" + "samsung.jpg", UriKind.Relative);
+                pic2.EndInit();
+                lblBrand.Content = "Brand  " + Samsung.Brand;
+                lblType.Content = "Type  " + Samsung.Type;
+                lblPower.Content = "Power   " + true;
+                lblHerz.Content = "Herz  " + Samsung.Herz;
+                lblImageSize.Content = "Image Size   " + Samsung.ImageSize;
+                lblChannel.Content = "Channel  " + Samsung.Channel;
+                lblVolume.Content = "Volume   " + Samsung.Volume;
+                img.Source = pic2;
+
+            }
+            else
+            {
+
+                lblPower.Content = false;
+            }
+
+
+        }
+
+        private void chkPower2_Checked(object sender, RoutedEventArgs e)
+        {
+            BitmapImage pic3 = new BitmapImage();
+            pic3.BeginInit();
+            pic3.UriSource = new Uri(@"Images\" + "philips.jpg", UriKind.Relative);
+            pic3.EndInit();
+            BitmapImage pic4 = new BitmapImage();
+            pic4.BeginInit();
+            pic4.UriSource = new Uri(@"Images\" + "tefal.jpg", UriKind.Relative);
+            pic4.EndInit();
+
+
+            if (cbSelection.SelectedIndex == 1)
+            {
+                img2.Source = pic3;
+                
+            }
+            else if(cbSelection.SelectedIndex ==0)
+            {
+
+                img2.Source = pic4;
+
+            }
         }
     }
 }
